61eba06793c4e84927e0720dcb0139b7fff0ab12
yuuki920 <yuuki.k.1672@gmail.com>
2020-04-27 16:46:45 +0900

    create_flash-message

--- config/application.rb
+++ config/application.rb
@@ -7,13 +7,15 @@ require 'rails/all'
 Bundler.require(*Rails.groups)
 
 module ChatSpace
-  class Application < Rails::Application
+  class Application < Rails::Application 
     config.generators do |g|
       g.stylesheets false
       g.javascripts false
       g.helper false
       g.test_framework false
+
     end
+    config.i18n.default_locale = :ja
     # Settings in config/environments/* take precedence over those specified here.
     # Application configuration should go into files in config/initializers
     # -- all .rb files in that directory are automatically loaded.
