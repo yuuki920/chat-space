23464d15b0562eec165ba7ae9afa136a0e9b7137
yuuki920 <yuuki.k.1672@gmail.com>
2020-05-01 12:39:07 +0900

    add_asynchronous_communication

---
+++ app/assets/javascripts/message.js
@@ -0,0 +1,69 @@
+$(function(){
+  function buildHTML(message){
+    if ( message.image) {
+      var html = 
+        `<div class="chat-contents">
+          <div class="chat-message-user">
+            <div class="chat-message-user__user-name">
+              ${message.user_name}
+            </div>
+            <div class="chat-message-user__day">
+              ${message.created_at}
+            </div>
+          </div>
+            <div class="chat-message-user-message">
+              <p class="chat-message-user-message__content">
+                ${message.content}
+              </p>
+              <img class="chat-message__image" src=${message.image}>
+            </div>   
+        </div>`
+        return html;
+    }   else {
+          var html =
+            `<div class="chat-contents">
+                <div class="chat-message-user">
+                  <div class="chat-message-user__user-name">
+                    ${message.user_name}
+                  </div>
+                  <div class="chat-message-user__day">
+                    ${message.created_at}
+                  </div>
+                </div>
+                <div class="chat-message-user-message">
+                  <p class="chat-message-user-message__content">
+                    ${message.content}
+                  </p>
+              </div>`
+          return html;
+        };
+    }
+  
+
+  $('#new_message').on ('submit', function(e){
+    e.preventDefault();   
+    var formData = new FormData(this);
+    var url = $(this).attr('action');
+    $.ajax({
+      url: url,
+      type:"POST",
+      data: formData,
+      dataType: 'json',
+      processData: false,
+      contentType: false
+    })
+    .done(function(data){
+      var html = buildHTML(data);
+      // $('.messages').append(html);
+      $('.chat-message').append(html);
+      $('form')[0].reset();
+      $('.chat-message').animate({ scrollTop: $('.chat-message')[0].scrollHeight});
+    })
+    .fail(function() {
+      alert("メッセージ送信に失敗しました");
+    })
+    .always(function() {
+      $(".submit-btn").prop('disabled', false);
+    });
+  })
+});
--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,9 +8,12 @@ class MessagesController < ApplicationController
   end
 
   def create
+    # binding.pry
     @message = @group.messages.new(message_params)
     if @message.save
-      redirect_to group_messages_path(@group), notice: 'メッセージが送信されました'
+      respond_to do |format|
+        format.json
+      end
     else
       @messages = @group.messages.includes(:user)
       flash.now[:alert] = 'メッセージを入力してください。'
