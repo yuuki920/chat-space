9e76b75245267232d9b9e5a58fb56d9ac3f0262f
yuuki920 <yuuki.k.1672@gmail.com>
2020-05-02 21:46:21 +0900

    fix_automatic_updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -69,7 +69,6 @@ $(function(){
     
     var reloadMessages = function(){
       var last_message_id = $('.chat-contents:last').data("message-id");
-      console.log(last_message_id)
       $.ajax({
         url:"api/messages",
         type:'get',
