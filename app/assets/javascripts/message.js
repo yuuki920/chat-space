3185c1c9b7c6bccf0c6404053d287db153fa253b
yuuki920 <yuuki.k.1672@gmail.com>
2020-05-02 21:51:08 +0900

    fix_automatic_updating

--- app/assets/javascripts/message.js
+++ app/assets/javascripts/message.js
@@ -76,7 +76,6 @@ $(function(){
         data: {id: last_message_id}
       })
       .done(function(messages){
-        console.log(messages)
         if (messages.length !==0){
           var insertHTML ='';
           $.each(messages,function(i, message){
